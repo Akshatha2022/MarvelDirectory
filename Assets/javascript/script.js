61b34baec09237e7ff319b45bbb09cdc600adc13
kristermyr <krister90@gmail.com>
2022-12-08 11:18:30 -0600

    changed so the name of the character is pulled from Marvel API and displayed on website

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -11,10 +11,10 @@ var getMarvelApi = function () {
       inputValue.value +
       `&ts=1&apikey=8e7128d8990f4d1a60535dfc20afb150&hash=f5ba5e9921f79729ed166c13294a9b6a`
   ).then(function (response) {
-    console.log(inputValue.value);
+    
     if (response.ok) {
       response.json().then(function (data) {
-        console.log(data.data.results[0].description);                        //navigates to the discription page in API and sets to short-info ID
+       console.log(data)                                             //navigates to the discription page in API and sets to short-info ID
         document.querySelector(".short-info").innerHTML =              
           data.data.results[0].description;
           
@@ -22,7 +22,9 @@ var getMarvelApi = function () {
           var characterImgURL = data.data.results[0].thumbnail.path;           //gets img from api and sets it in ID of img element
           var characterImg = [characterImgURL+"/portrait_incredible"+ ".jpg"];
           $('#characterImg').attr('src', characterImg);  
-          
+          document.querySelector("#name").innerHTML =              
+          data.data.results[0].name;
+
       });
     }
     
